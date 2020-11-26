feb1ede281684ad499fb865fc77535a7348ea943
Hermer29 <kliman.chan@gmail.com>
2020-11-26 11:33:14 +0300

    Update unauth.js
    Added minor comment in unauth.js file

--- js/unauth.js
+++ js/unauth.js
@@ -1,7 +1,9 @@
-$(function () {
+$(function() {
     $("button#unauth").on("click", () => {
+        //Sets cookies expire time to past date:
         document.cookie = "login= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
         document.cookie = "password= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+        //and reload page
         window.location.reload();
     });
 });
